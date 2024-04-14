e1f2b29bc4f7fbdc21a3eddf0e9d23a15d7911d7
mgismissing <gabrycraft2010@gmail.com>
2024-04-14 18:57:37 +0200

    Last edit

--- monday.js
+++ monday.js
@@ -33,3 +33,52 @@ function MondayGetItemCount(boardId) {
         console.error('There was a problem with your fetch operation:', error);
     });
 }
+function MondayGetItemCountTextFilter(boardId, textFilter) {
+    // Define your API key and board ID
+    const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM0NjYyNDg5MCwiYWFpIjoxMSwidWlkIjo1ODg4MTMwMiwiaWFkIjoiMjAyNC0wNC0xM1QyMjoxNDozNi4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjI2ODUzNTgsInJnbiI6ImV1YzEifQ.vVl92pM2Tf7-g4h2n9gSm4pXxbdUS-3r5lM_YOgNhz4';
+
+    // Define the endpoint for querying items in the board
+    const endpoint = `https://api.monday.com/v2`;
+
+    // Define your request headers
+    const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': apiKey
+    };
+
+    // Define the query
+    const query = `
+    query {
+        boards (ids: ${boardId}) {
+        items_by_column_values (column_values: {text: "${textFilter}"}, limit: 1) {
+            count
+        }
+        }
+    }
+    `;
+
+    // Define your Fetch options
+    const options = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ query: query })
+    };
+
+    // Make the API request
+    fetch(endpoint, options)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+    })
+    .then(data => {
+        // Handle the API response data
+        const count = data.data.boards[0].items_by_column_values[0].count;
+        return count;
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('There was a problem with your fetch operation:', error);
+    });
+
+}
--- qupdate.js
+++ qupdate.js
@@ -1,20 +1,52 @@
 // Define the qinfo file
-var totalCount = 3;
+const totalCount = 3;
+const occupiedTotal = 3;
+const occupiedPrio = 0;
+const occupiedFixed = 2;
+const linesRaw = [
+  totalCount-occupiedTotal,
+  totalCount-occupiedPrio-occupiedFixed,
+  occupiedTotal,
+  occupiedPrio+occupiedFixed,
+  totalCount
+];
+var linesCond1 = 'err0';
+if (linesRaw[2] == linesRaw[4]) {
+  if (linesRaw[3] == linesRaw[4]) {
+    linesCond1 = 'full';
+  } else {
+    linesCond1 = 'prio';
+  }
+} else if (linesRaw[2] == linesRaw[4]-1) {
+  linesCond1 = 'tbfl';
+} else if (linesRaw[2] < linesRaw[4]-1) {
+  linesCond1 = 'avlb';
+};
+var linesCond2 = 'err0';
+if (linesRaw[2] == linesRaw[4]) {
+  if (linesRaw[3] == linesRaw[4]) {
+    linesCond2 = 'full';
+  } else {
+    linesCond2 = 'prio';
+  }
+} else if (linesRaw[2] < linesRaw[4]-1) {
+  linesCond2 = 'avlb';
+}
 const lines = [
   'Posti liberi senza priorità [int]',
-  totalCount-MondayGetItemCount('1462254747'),
+  linesRaw[0],
   'Posti liberi con priorità [int]',
-  0,
+  linesRaw[1],
   'Posti occupati senza priorità [int]',
-  MondayGetItemCount('1462254747'),
+  linesRaw[2],
   'Posti occupati con priorità [int]',
-  3,
+  linesRaw[3],
   'Posti totali [int]',
-  totalCount,
-  'Stato coda [dropdown] (full "full" / tbfl "to be full" / avlb "available" / prio "available only for priority)',
-  'full',
-  'Stato piazzamento nuovi ordini [dropdown] (full "full" / avlb "available")',
-  'full',
+  linesRaw[4],
+  'Stato coda [dropdown] (full "full" / tbfl "to be full" / avlb "available" / prio "available only for priority")',
+  linesCond1,
+  'Stato piazzamento nuovi ordini [dropdown] (full "full" / avlb "available" / prio "available only for priority")',
+  linesCond2,
   'Fine documento informazioni',
 ];
 
@@ -27,11 +59,13 @@ try {
   document.getElementById('queuestate-'+lines[11]).removeAttribute('hidden');
   document.getElementById('queuestate-err0').setAttribute('hidden', true);
 } catch (error) {
+  console.error('Value "queuestate-'+lines[11]+'" is not valid.');
   console.error(error);
 };
 try {
   document.getElementById('buy-'+lines[13]).removeAttribute('hidden');
   document.getElementById('buy-err0').setAttribute('hidden', true);
 } catch (error) {
+  console.error('Value "buy-'+lines[13]+'" is not valid.');
   console.error(error);
 }
